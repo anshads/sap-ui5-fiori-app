d46529be5cfb3456b5ab4623ff8637818b8836df
anshads <anshad.vp@marlabs.com>
2014-04-01 11:20:36 +0530

    Added serverside REStful communication method

--- Component.js
+++ Component.js
@@ -2,7 +2,7 @@
 * @Author: anshad.vp
 * @Date:   2014-03-28 12:06:30
 * @Last Modified by:   anshad.vp
-* @Last Modified time: 2014-03-28 16:40:08
+* @Last Modified time: 2014-04-01 11:14:55
 */
 
 jQuery.sap.declare("sap.ui.demo.app.Component");
@@ -34,6 +34,31 @@ sap.ui.core.UIComponent.extend("sap.ui.demo.app.Component", {
 		}); 
 		deviceModel.setDefaultBindingMode("OneWay"); 
 		oView.setModel(deviceModel, "device");
+
+		/*
+		sap.ui.controller("sapui5_test.SAPUI5_Test", {
+
+		 initTodoModel : function() {  
+		        var oModel = new sap.ui.model.json.JSONModel();
+		        var aData = jQuery.ajax({
+		            type : "GET",
+		            contentType : "application/json",
+		            url : "http://sapm04.ibsolution.local:50000/demo.sap.com~d337_resttest_web/rest/todo/init/",
+		            dataType : "json",
+		            async: false, 
+		            success : function(data,textStatus, jqXHR) {
+		                oModel.setData({modelData : data}); 
+		                alert("success to post");
+		            }
+
+		        });
+
+		        return oModel;  
+		    }
+		  }
+
+		 });
+		*/
 
 		/*// Using OData model to connect against a real service
 		var url = "/proxy/http/<server>:<port>/sap/opu/odata/sap/ZGWSAMPLE_SRV/";
